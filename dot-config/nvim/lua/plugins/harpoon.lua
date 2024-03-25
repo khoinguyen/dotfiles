b088be9000e27a19549cc2c3a8296854f872fd64
Khoi Nguyen <khoi.nqq@gmail.com>
2024-03-26 10:47:57 +1100

    fix(harpoon): use simple built-in harpoon quick menu
    - Telescope harpoon menu not re-numbering the harpoon list, which cannot use nav_next to loop around the marked files

--- dot-config/nvim/lua/plugins/harpoon.lua
+++ dot-config/nvim/lua/plugins/harpoon.lua
@@ -5,7 +5,7 @@ return {
   },
   config = function()
     require("telescope").load_extension('harpoon')
-    vim.keymap.set('n', '<leader>\\', ':Telescope harpoon marks<CR>', {})
+    vim.keymap.set('n', '<leader>\\', ':lua require("harpoon.ui").toggle_quick_menu()<CR>', {})
     vim.keymap.set('n', '<leader>/', ':lua require("harpoon.mark").add_file()<CR>', {})
     vim.keymap.set('n', '<leader>n', ':lua require("harpoon.ui").nav_next()<CR>', {})
   end
