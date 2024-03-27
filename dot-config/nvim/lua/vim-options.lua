14a5e139a88f2769384826f8cc62afeb81333825
Khoi Nguyen <khoi.nqq@gmail.com>
2024-03-27 13:49:36 +1100

    feat: make cursor more visible

--- dot-config/nvim/lua/vim-options.lua
+++ dot-config/nvim/lua/vim-options.lua
@@ -4,5 +4,8 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set nu rnu")
 vim.cmd("set splitright")
-
+vim.cmd("nnoremap j jzz")
+vim.cmd("nnoremap k kzz")
+vim.opt.cursorline = true
+vim.opt.cursorcolumn = true
 vim.g.mapleader = " "
