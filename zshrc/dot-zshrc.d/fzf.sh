ce0eee06dee49fa12b9630d0ae8687c33130a350
Khoi Nguyen <khoi.nqq@gmail.com>
2025-09-01 11:09:43 +1000

    add fzf tmux opts

--- zshrc/dot-zshrc.d/fzf.sh
+++ zshrc/dot-zshrc.d/fzf.sh
@@ -11,6 +11,7 @@ export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude .git"
 
+export FZF_TMUX_OPTS=" -p90%,70% "
 # Use fd (https://github.com/sharkdp/fd) for listing path candidates.
 # - The first argument to the function ($1) is the base path to start traversal
 # - See the source code (completion.{bash,zsh}) for the details.
